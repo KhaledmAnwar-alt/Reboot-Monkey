d31905d1b36760b9901cc08cb4f827f5cadb9ebc
eDoubleV <113300200+eDoubleV@users.noreply.github.com>
2022-12-24 12:19:36 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const navE1 = document.querySelector('.navbar');
+window.addEventListener('scroll', () => {
+  if (window.scrollY >= 56) {
+    navE1.classList.add('navbar-scrolled');
+  } else if (window.scrollY < 56) {
+    navE1.classList.remove('navbar-scrolled');
+  }
+});
